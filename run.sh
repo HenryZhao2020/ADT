18c738d5f144ab9bbdeee4441ad9086a1a7f0987
Henry Zhao <henryzhao2019@gmail.com>
2025-04-23 05:50:03 +0000

    Updated testing

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ LEAK_CHECK=true
 clear
 clang -std=c99 -g -Wall -Wextra alist.c datatype.c main.c -o $execname
 if [ $? -eq 0 ]; then
-  ./$execname > test.out
-  diff test.out test.expect
-
   if [ "$LEAK_CHECK" = true ]; then
     echo "Checking for memory leaks with valgrind..."
     valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./$execname
   fi
+
+  ./$execname > test.out
+  diff test.out test.expect
 else
   echo "Compilation failed."
 fi
